5b6480047000f1c8d7f0f48d1ed3a26f1a26161e
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-06-04 15:14:53 +0200

    feat: fight matches until last fighter, set timeout between events

--- script.js
+++ script.js
@@ -1,112 +1,37 @@
 const fighters = [
-  {
-    name: "Freezer",
-    power: 8000,
-  },
-  {
-    name: "Vegeta",
-    power: 8500,
-  },
-  {
-    name: "Crilin",
-    power: 500,
-  },
-  {
-    name: "Mr Satan",
-    power: 50,
-  },
-  {
-    name: "Junior",
-    power: 6000,
-  },
-  {
-    name: "Goku",
-    power: 9001,
-  },
-  {
-    name: "Tensing",
-    power: 450,
-  },
-  {
-    name: "Videl",
-    power: 300,
-  },
-  {
-    name: "Bulma",
-    power: 20,
-  },
-  {
-    name: "C-18",
-    power: 7800,
-  },
-  {
-    name: "Gohan",
-    power: 8900,
-  },
-  {
-    name: "Trunks",
-    power: 1250,
-  },
+  { name: "Freezer", power: 8000 },
+  { name: "Vegeta", power: 8500 },
+  { name: "Crilin", power: 500 },
+  { name: "Mr Satan", power: 50 },
+  { name: "Junior", power: 6000 },
+  { name: "Goku", power: 9001 },
+  { name: "Tensing", power: 450 },
+  { name: "Videl", power: 300 },
+  { name: "Bulma", power: 20 },
+  { name: "C-18", power: 7800 },
+  { name: "Gohan", power: 8900 },
+  { name: "Trunks", power: 1250 },
 ];
 
 const weapons = [
-  {
-    name: "Ventaglio della Musa",
-    power: 15,
-  },
-  {
-    name: "Scouter",
-    power: 30,
-  },
-  {
-    name: "Bastone Roshi",
-    power: 60,
-  },
-  {
-    name: "Fagioli Magici",
-    power: 70,
-  },
-  {
-    name: "Katana di Yajirobei",
-    power: 85,
-  },
-  {
-    name: "Spada del Dragone Azzurro",
-    power: 115,
-  },
-  {
-    name: "Armatura Saiyan",
-    power: 145,
-  },
-  {
-    name: "Cannone da braccio",
-    power: 170,
-  },
-  {
-    name: "Nuvola d'oro",
-    power: 200,
-  },
-  {
-    name: "Bastone Nyoi",
-    power: 220,
-  },
-  {
-    name: "Spada Z",
-    power: 235,
-  },
-  {
-    name: "Orecchini Potara",
-    power: 250,
-  },
+  { name: "Ventaglio della Musa", power: 15 },
+  { name: "Scouter", power: 30 },
+  { name: "Bastone Roshi", power: 60 },
+  { name: "Fagioli Magici", power: 70 },
+  { name: "Katana di Yajirobei", power: 85 },
+  { name: "Spada del Dragone Azzurro", power: 115 },
+  { name: "Armatura Saiyan", power: 145 },
+  { name: "Cannone da braccio", power: 170 },
+  { name: "Nuvola d'oro", power: 200 },
+  { name: "Bastone Nyoi", power: 220 },
+  { name: "Spada Z", power: 235 },
+  { name: "Orecchini Potara", power: 250 },
 ];
 
 let fightersWithWeapons = [];
 let trainedFightersWithWeapons = [];
 let filteredTrainedFighters = [];
 let winningFighters = [];
-
-console.table(fighters);
-console.table(weapons);
 
 // milestone 1 scelta dell'arma
 function ChooseWeapon(weapons, fighters) {
@@ -159,45 +84,109 @@ function FilterFighter() {
 }
 
 // milestone 4 - combattimento -> pg con potenza maggiore vince
-function checkFightersNumber() {
-  if (filteredTrainedFighters.length % 2 !== 0) {
+function checkFightersNumber(array) {
+  if (array.length % 2 !== 0) {
     const fighterBot = {
       name: "Broly",
       power: 4000,
 
       trainedPower: 4000 * Math.floor(Math.random() * 101),
     };
-    filteredTrainedFighters.push(fighterBot);
-    console.table(filteredTrainedFighters);
+    array.push(fighterBot);
+    // console.table(array);
   }
 }
-function fight() {
-  checkFightersNumber();
-  while (filteredTrainedFighters.length > 1) {
-    let fighter1 = filteredTrainedFighters[0];
-    let fighter2 = filteredTrainedFighters[1];
+function fight(array) {
+  checkFightersNumber(array);
+
+  console.table(array);
+  let roundWinners = [];
+
+  while (array.length > 1) {
+    let fighter1 = array.shift();
+    let fighter2 = array.shift();
 
     if (fighter1.trainedPower > fighter2.trainedPower) {
-      console.log(`${fighter1.name} ha vinto! contro ${fighter2.name}`);
-      winningFighters.push(fighter1);
+      console.log(`${fighter1.name} ha vinto contro ${fighter2.name}`);
+      roundWinners.push(fighter1);
     } else if (fighter1.trainedPower < fighter2.trainedPower) {
-      console.log(`${fighter2.name} ha vinto! contro ${fighter1.name}`);
-      winningFighters.push(fighter2);
+      console.log(`${fighter2.name} ha vinto contro ${fighter1.name}`);
+      roundWinners.push(fighter2);
     } else {
-      console.log(`Pareggio!`);
+      console.log(`Pareggio! Nessuno avanza.`);
     }
-    filteredTrainedFighters.splice(0, 2);
   }
+
+  if (array.length === 1) {
+    roundWinners.push(array[0]);
+  }
+  return roundWinners;
 }
 
-ChooseWeapon(weapons, fighters);
-// console.table(fightersWithWeapons);
+// milestone 5 - premiazione -> i combattenti con il livello di forza maggiore arrivano sul podio
+function podium(array) {
+  array.sort((a, b) => b.trainedPower - a.trainedPower);
+  console.table(array);
 
-TrainingFighter();
-// console.table(trainedFightersWithWeapons);
+  let winner = array[0];
+  let second = array[1];
+  let third = array[2];
+  if (winner) console.log(`${winner.name} ha vinto il podio! 1`);
+  if (second) console.log(`${second.name} ha vinto il podio! 2`);
+  if (third) console.log(`${third.name} ha vinto il podio! 3`);
+}
 
-FilterFighter();
-console.table(filteredTrainedFighters);
+function startTournament() {
+  console.warn(`======================Fighter List======================`);
+  console.table(fighters);
 
-fight();
-console.table(winningFighters);
+  console.warn(`======================Weapon List======================`);
+  console.table(weapons);
+
+  setTimeout(() => {
+    console.warn(`======================Getting Weapons======================`);
+    ChooseWeapon(weapons, fighters);
+    console.table(fightersWithWeapons);
+
+    setTimeout(() => {
+      console.warn(`======================Training======================`);
+      TrainingFighter();
+      console.table(trainedFightersWithWeapons);
+
+      setTimeout(() => {
+        console.warn(
+          `======================Qualification======================`
+        );
+        FilterFighter();
+        console.table(filteredTrainedFighters);
+
+        let i = 0;
+        function nextFightRound() {
+          if (filteredTrainedFighters.length > 3) {
+            i++;
+            console.warn(
+              `======================Round ${i}======================`
+            );
+            filteredTrainedFighters = fight(filteredTrainedFighters);
+            winningFighters = [...filteredTrainedFighters];
+            setTimeout(nextFightRound, 2000);
+          } else if (filteredTrainedFighters.length > 1) {
+            i++;
+            console.warn(
+              `======================Round ${i}======================`
+            );
+            filteredTrainedFighters = fight(filteredTrainedFighters);
+            setTimeout(nextFightRound, 2000);
+          } else {
+            console.warn(`======================Podium======================`);
+            podium(winningFighters);
+          }
+        }
+
+        nextFightRound();
+      }, 1000);
+    }, 2000);
+  }, 2000);
+}
+
+startTournament();
