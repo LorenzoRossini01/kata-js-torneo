d9052e77791db62e3cbc6b27a75e9abdd718c9e9
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-06-07 19:57:24 +0200

    feat: style, structure and logic

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Arrays
 const fighters = [
   { name: "Freezer", power: 8000 },
   { name: "Vegeta", power: 8500 },
@@ -28,51 +29,104 @@ const weapons = [
   { name: "Orecchini Potara", power: 250 },
 ];
 
+const fightersImages = [
+  "img/freezer.jpg",
+  "img/vegeta.png",
+  "img/crilin.webp",
+  "img/mrsatan.jpeg",
+  "img/piccolo.jpg",
+  "img/goku.jpg",
+  "img/tensing.webp",
+  "img/videl.webp",
+  "img/bulma.webp",
+  "img/c18.jpg",
+  "img/gohan",
+  "img/trunks.jpg",
+];
+
+weaponsImages = [
+  "img/ventagliomusa.webp",
+  "img/scouter.webp",
+  "img/roshi.webp",
+  "img/fagioli.webp",
+  "img/katana.webp",
+  "img/dragoneazzurro.webp",
+  "img/armatura sayan.webp",
+  "img/cannone.webp",
+  "img/nuvola.webp",
+  "img/bastone.webp",
+  "img/spadaz.jpg",
+  "img/potara.webp",
+];
+
 let fightersWithWeapons = [];
-let trainedFightersWithWeapons = [];
+let tranedFighters = [];
 let filteredTrainedFighters = [];
 let winningFighters = [];
+let roundWinners = [];
+let isTrained = false;
+let afterFirstRound = false;
+let podiumList = [];
 
+// dom selectors
 const startButton = document.getElementById("start-tournament");
 const TrainButton = document.getElementById("train-button");
+const WeaponButton = document.getElementById("weapon-button");
 const nextFightButton = document.getElementById("next-fight");
+const nextFightContainer = document.getElementById("next-fight-container");
+nextFightContainer.classList.add("hidden");
 const TournamentCharacterSection = document.getElementById(
   "tournament-characters"
 );
 const TournamentWeaponsrSection = document.getElementById("tournament-weapons");
 const TournamentBrackets = document.getElementById("tournament-brackets");
+const roundWinnersList = document.getElementById("round-winners");
+const TournamentBracketsRow = document.querySelector(".tournament-brackets");
+const podiumListElement = document.getElementById("podium-list");
 
-fighters.forEach((fighter) => {
-  TournamentCharacterSection.innerHTML += `
-  <div class="col">
+// aggiunta immagine all'array
+function addImgToArray(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    array2[i].image = array1[i];
+  }
+
+  console.table(fighters);
+}
+
+// stampa card in pagina
+function printCard(element, item) {
+  if (item.weaponPower) {
+    element.innerHTML += `
+    <div class="col">
+      <div class="img-wrapper bg-light">
+        <img src="${item.image}" alt="${item.name}" />
+        <div class="img-overlay">
+          <h2 class="card-text">${item.name}</h2>
+          <div class="card-detail">
+            <h2 class="card-text">${item.name}</h2>
+            <p><strong>Final Power:</strong> ${item.power}</p>
+            <p><strong>Weapon:</strong> ${item.weaponName}</p>
+            <p><strong>Weapon Power:</strong> ${item.weaponPower}</p>
+          </div>
+        </div>
+      </div>
+    </div>`;
+  } else {
+    element.innerHTML += `
+    <div class="col">
   <div class="img-wrapper bg-light">
-  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsb3ipZexAsmxnHO_6FDIAKUA7aOovFj4p0A&s" />
+  <img src="${item.image}"  alt="${item.name}"/>
   <div class="img-overlay">
-  <h2 class="card-text">${fighter.name}</h2>
+  <h2 class="card-text">${item.name}</h2>
   <div class="card-detail">
-  <h2 class="card-text">${fighter.name}</h2>
-  <p><strong>Power:</strong> ${fighter.power}</p>
+  <h2 class="card-text">${item.name}</h2>
+  <p><strong>Power:</strong> ${item.power}</p>
   </div>
   </div>
   </div>
   </div>`;
-});
-weapons.forEach((weapon) => {
-  TournamentWeaponsrSection.innerHTML += `
-  <div class="col">
-  <div class="img-wrapper bg-light">
-  <img src="https://i.pinimg.com/originals/92/b4/97/92b49766c818b5fac6e8781913fd13f3.png" />
-
-  <div class="img-overlay">
-  <h2 class="card-text">${weapon.name}</h2>
-  <div class="card-detail">
-  <h2 class="card-text">${weapon.name}</h2>
-  <p><strong>Power:</strong> ${weapon.power}</p>
-  </div>
-  </div>
-  </div>
-  </div>`;
-});
+  }
+}
 
 // milestone 1 scelta dell'arma
 function ChooseWeapon(weapons, fighters) {
@@ -92,6 +146,7 @@ function ChooseWeapon(weapons, fighters) {
   fightersWithWeapons = fighters.map((fighter, i) => {
     return {
       ...fighter,
+      power: fighter.power + shuffledWeapons[i].power,
       weaponIndex: weapons.findIndex(
         (weapon) => weapon.name === shuffledWeapons[i].name
       ),
@@ -99,46 +154,68 @@ function ChooseWeapon(weapons, fighters) {
       weaponPower: shuffledWeapons[i].power,
     };
   });
+
+  TournamentCharacterSection.innerHTML = "";
+  fightersWithWeapons.forEach((fighter) => {
+    printCard(TournamentCharacterSection, fighter);
+    // TournamentBracketsSection.;
+  });
 }
 
 // milestone 2 allenamento fighters
 function TrainingFighter() {
-  trainedFightersWithWeapons = fightersWithWeapons.map((fighter) => ({
-    ...fighter,
-    trainedPower:
-      (fighter.power + fighter.weaponPower) * Math.floor(Math.random() * 101),
-  }));
+  if (fightersWithWeapons.length > 0) {
+    tranedFighters = fightersWithWeapons.map((fighter) => ({
+      ...fighter,
+      power:
+        fighter.power + fighter.weaponPower * Math.floor(Math.random() * 101),
+    }));
+    if (roundWinners.length > 0) {
+      roundWinners = roundWinners.map((fighter) => ({
+        ...fighter,
+        power:
+          fighter.power + fighter.weaponPower * Math.floor(Math.random() * 101),
+      }));
+    }
+  } else {
+    tranedFighters = fighters.map((fighter) => ({
+      ...fighter,
+      power: fighter.power * Math.floor(Math.random() * 101),
+    }));
+    if (roundWinners.length > 0) {
+      roundWinners = roundWinners.map((fighter) => ({
+        ...fighter,
+        power:
+          fighter.power + fighter.weaponPower * Math.floor(Math.random() * 101),
+      }));
+    }
+  }
+
   TournamentCharacterSection.innerHTML = "";
-  trainedFightersWithWeapons.forEach((fighter) => {
-    TournamentCharacterSection.innerHTML += `
-    <div class="col">
-    <div class="img-wrapper bg-light">
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsb3ipZexAsmxnHO_6FDIAKUA7aOovFj4p0A&s" />
-    <div class="img-overlay">
-    <h2 class="card-text">${fighter.name}</h2>
-    <div class="card-detail trained">
-    <h2 class="card-text">${fighter.name}</h2>
-    <p><strong>Power:</strong> ${fighter.power}</p>
-    <p><strong>Weapon:</strong> ${fighter.weaponName}</p>
-    <p><strong>Weapon Power:</strong> ${fighter.weaponPower}</p>
-    <p><strong>Final Power:</strong> ${fighter.trainedPower}</p>
-    </div>
-    </div>
-    </div>
-    </div>`;
+  tranedFighters.forEach((fighter) => {
+    printCard(TournamentCharacterSection, fighter);
+    // TournamentBracketsSection.;
   });
+
+  isTrained = true;
 }
 
 // milestone 3 qualificazione -> filtraggio dei fighters
 function FilterFighter() {
-  filteredTrainedFighters = trainedFightersWithWeapons.filter(
-    (fighter) => fighter.trainedPower >= 10000
-  );
+  if (isTrained) {
+    filteredTrainedFighters = tranedFighters.filter(
+      (fighter) => fighter.power >= 10000
+    );
+  } else {
+    filteredTrainedFighters = fighters.filter(
+      (fighter) => fighter.power >= 2000
+    );
+  }
   filteredTrainedFighters = filteredTrainedFighters.map((fighter) => {
     return {
       name: fighter.name,
       power: fighter.power,
-      trainedPower: fighter.trainedPower,
+      image: fighter.image,
     };
   });
 }
@@ -149,6 +226,7 @@ function checkFightersNumber(array) {
     const fighterBot = {
       name: "Broly",
       power: 4000,
+      image: "img/broly.png",
 
       trainedPower: 4000 * Math.floor(Math.random() * 101),
     };
@@ -161,7 +239,10 @@ function fight(array) {
 
   console.table(array);
 
-  let roundWinners = [];
+  startButton.classList.add("hidden");
+
+  roundWinners = [];
+  roundWinnersList.innerHTML = "";
   TournamentBrackets.innerHTML = "";
   while (array.length > 1) {
     let fighter1 = array.shift();
@@ -169,66 +250,84 @@ function fight(array) {
 
     TournamentBrackets.innerHTML += `            
     <li class="col">
-      <div
-        class="bg-light text-center d-flex justify-content-center align-items-center gap-3"
-      >
-      <div class="img-wrapper">
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsb3ipZexAsmxnHO_6FDIAKUA7aOovFj4p0A&s"
-          alt=""
-        />
-        <div class="img-overlay">
-          <h3>${fighter1.name}</h3>
+      <div class="bg-light text-center d-flex justify-content-center align-items-center gap-3">
+        <div class="img-wrapper" id="${fighter1.name}">
+          <img src="${fighter1.image}" alt="" class="img-fluid" />
+          <div class="img-overlay">
+            <h3>${fighter1.name}</h3>
+          </div>
+        </div>
+        <h3>VS</h3>
+        <div class="img-wrapper" id="${fighter2.name}">
+          <img src="${fighter2.image}" alt="" class="img-fluid" />
+          <div class="img-overlay">
+            <h3>${fighter2.name}</h3>
+          </div>
         </div>
       </div>
-      <h3>VS</h3>
-      <div class="img-wrapper">
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsb3ipZexAsmxnHO_6FDIAKUA7aOovFj4p0A&s"
-          alt=""
-        />
-        <div class="img-overlay">
-          <h3>${fighter2.name}</h3>
-        </div>
-      </div>
-    </div>
-  </li>`;
+    </li>`;
 
-    if (fighter1.trainedPower > fighter2.trainedPower) {
-      console.log(`${fighter1.name} ha vinto contro ${fighter2.name}`);
-      roundWinners.push(fighter1);
-    } else if (fighter1.trainedPower < fighter2.trainedPower) {
+    if (fighter1.power < fighter2.power) {
       console.log(`${fighter2.name} ha vinto contro ${fighter1.name}`);
+      let loser = document.getElementById(fighter1.name);
+      loser.classList.add("loser");
       roundWinners.push(fighter2);
-    } else if (fighter1.power < fighter2.power) {
-      console.log(`${fighter2.name} ha vinto contro ${fighter1.name}`);
-      roundWinners.push(fighter2);
+      podiumList.push(fighter1);
     } else if (fighter1.power > fighter2.power) {
       console.log(`${fighter1.name} ha vinto contro ${fighter2.name}`);
+      let loser = document.getElementById(fighter2.name);
+      loser.classList.add("loser");
       roundWinners.push(fighter1);
+      podiumList.push(fighter2);
     } else {
       console.log(`Pareggio! Nessuno avanza.`);
     }
   }
-
+  roundWinners.forEach((winner) => {
+    printCard(roundWinnersList, winner);
+  });
   if (array.length === 1) {
+    nextFightContainer.classList.add("hidden");
     roundWinners.push(array[0]);
   }
+
   return roundWinners;
 }
 
 // milestone 5 - premiazione -> i combattenti con il livello di forza maggiore arrivano sul podio
 function podium(array) {
-  array.sort((a, b) => b.trainedPower - a.trainedPower);
-  console.table(array);
+  array.reverse();
+  console.log(array);
+  // array.sort((a, b) => b.trainedPower - a.trainedPower);
+  // console.table(array);
 
-  let winner = array[0];
-  let second = array[1];
-  let third = array[2];
+  let winner = roundWinners[0];
+  let second = array[0];
+  let third = array[1];
+
   if (winner) console.log(`${winner.name} ha vinto il podio! 1`);
   if (second) console.log(`${second.name} ha vinto il podio! 2`);
   if (third) console.log(`${third.name} ha vinto il podio! 3`);
+
+  const podiumElement = document.getElementById("podium");
+  podiumElement.classList.remove("hidden");
+  printCard(podiumListElement, second);
+  printCard(podiumListElement, winner);
+  printCard(podiumListElement, third);
 }
+
+// #inizio torneo
+// aggiunta immagini a fighters e weapons
+addImgToArray(fightersImages, fighters);
+addImgToArray(weaponsImages, weapons);
+
+// stampa in pagina lista fighters e weapons
+fighters.forEach((fighter) => {
+  printCard(TournamentCharacterSection, fighter);
+});
+weapons.forEach((weapon) => {
+  printCard(TournamentWeaponsrSection, weapon);
+});
 
 function startTournament() {
   console.warn(`======================Fighter List======================`);
@@ -237,42 +336,28 @@ function startTournament() {
   console.warn(`======================Weapon List======================`);
   console.table(weapons);
 
-  // setTimeout(() => {
-  //   console.warn(`======================Getting Weapons======================`);
-  //   ChooseWeapon(weapons, fighters);
-  //   console.table(fightersWithWeapons);
+  console.warn(`======================Qualification======================`);
+  FilterFighter();
+  console.table(filteredTrainedFighters);
 
-  //   setTimeout(() => {
-  //     console.warn(`======================Training======================`);
-  //     TrainingFighter();
-  //     console.table(trainedFightersWithWeapons);
+  let i = 0;
+  nextFightContainer.classList.remove("hidden");
 
-  setTimeout(() => {
-    console.warn(`======================Qualification======================`);
-    FilterFighter();
-    console.table(filteredTrainedFighters);
+  if (filteredTrainedFighters.length > 3) {
+    i++;
+    console.warn(`======================Round ${i}======================`);
+    filteredTrainedFighters = fight(filteredTrainedFighters);
+    winningFighters = [...filteredTrainedFighters];
+  } else if (filteredTrainedFighters.length > 1) {
+    i++;
+    nextFightContainer.classList.add("hidden");
 
-    let i = 0;
-    function nextFightRound() {
-      if (filteredTrainedFighters.length > 3) {
-        i++;
-        console.warn(`======================Round ${i}======================`);
-        filteredTrainedFighters = fight(filteredTrainedFighters);
-        winningFighters = [...filteredTrainedFighters];
-      } else if (filteredTrainedFighters.length > 1) {
-        i++;
-        console.warn(`======================Round ${i}======================`);
-        filteredTrainedFighters = fight(filteredTrainedFighters);
-      } else {
-        console.warn(`======================Podium======================`);
-        podium(winningFighters);
-      }
-    }
-
-    nextFightRound();
-  }, 1000);
-  //   }, 2000);
-  // }, 2000);
+    console.warn(`======================Round ${i}======================`);
+    filteredTrainedFighters = fight(filteredTrainedFighters);
+  } else {
+    console.warn(`======================Podium======================`);
+    podium(podiumList);
+  }
 }
 
 startButton.addEventListener("click", function () {
@@ -280,15 +365,23 @@ startButton.addEventListener("click", function () {
 });
 
 nextFightButton.addEventListener("click", function () {
-  // Esegui il combattimento
-  filteredTrainedFighters = fight(filteredTrainedFighters);
-});
+  if (filteredTrainedFighters.length > 1) {
+    nextFightContainer.classList.remove("hidden");
 
-TrainButton.addEventListener("click", function () {
+    // Esegui il combattimento
+    filteredTrainedFighters = fight(filteredTrainedFighters);
+  } else {
+    nextFightContainer.classList.add("hidden");
+    podium(podiumList);
+  }
+});
+WeaponButton.addEventListener("click", function () {
   console.warn(`======================Getting Weapons======================`);
   ChooseWeapon(weapons, fighters);
   console.table(fightersWithWeapons);
+});
+TrainButton.addEventListener("click", function () {
   console.warn(`======================Training======================`);
   TrainingFighter();
-  console.table(trainedFightersWithWeapons);
+  console.table(tranedFighters);
 });
